5c649c2f7d490c7053dc7595dca2aae4d9477411
Vallipibharu01 <v.bhargavi2001@gmail.com>
2023-11-21 22:33:57 +0530

    first commit

---
+++ app/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+
+# Create your views here.
+def bharu(request):
+    return render(request,'bharu.html')
+
+def amma(request):
+    return render(request,'amma.html')
